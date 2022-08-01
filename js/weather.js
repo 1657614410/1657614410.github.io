02c337247a27400da7be7c78f7c309fa848097d0
1657614410 <1657614410@qq.com>
2022-08-01 20:39:19 +0800

    Site updated: 2022-08-01 20:39:11

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "18",
+    "cityColor": "FFFFFF",
+    "citySize": "18",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "0",
+    "weatherIconSize": "25",
+    "alertIconSize": "18",
+    "padding": "10px 5px 10px 5px",
+    "shadow": "0",
+    "language": "zh",
+    "borderRadius": "5",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "city": "CN101220606",
+    "left": "175",
+    "top": "10",
+    "key": "0c466313b2294d32acd1cb7bc482de0e"
+  }
+}
---
+++ js/yejiao.js
@@ -0,0 +1,9 @@
+// 动态心跳
+$(document).ready(function(e){
+    $('.copyright').html('©2019 - 2022 <i class="fa-fw fas fa-heartbeat fa-beat" style="color: #f92672;"></i> By 爱rua猫的李同学');
+})
+
+$(document).ready(function(e){
+    show_date_time();
+})
+
